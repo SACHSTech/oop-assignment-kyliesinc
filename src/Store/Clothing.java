8175f6bb1918eeb361ff884e49299e495891e968
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2022-11-17 11:59:15 -0500

    clothing size

--- src/Store/Clothing.java
+++ src/Store/Clothing.java
@@ -4,23 +4,17 @@ package Store;
 public abstract class Clothing {
     // private variables 
     private String type;
-    private int size;
     private String colour;
     private double price;
 
-    public Clothing(String clothType, int clothSize, String clothColour, double clothPrice){
+    public Clothing(String clothType, String clothColour, double clothPrice){
         type = clothType;
-        size = clothSize;
         colour = clothColour;
         price = clothPrice;
     }
 
     public String getType(){
         return type;
-    }
-
-    public int getSize() {
-        return size;
     }
 
     public String getColour() {
