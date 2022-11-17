e92075c557a2ffbd4bcc7ed8ad894dc31e080e30
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2022-11-16 21:39:48 -0500

    added basics

---
+++ src/Store/Clothing.java
@@ -0,0 +1,24 @@
+package Store;
+import oop.*;
+
+
+public class Clothing {
+    // private variables 
+    private String size;
+    private String colour;
+
+    public Clothing(String clothSize, String clothColour){
+        size = clothSize;
+        colour = clothColour;
+    }
+
+    public String getSize({
+        return size;
+    }
+
+    public String getColour({
+        return colour;
+    }
+
+
+}
