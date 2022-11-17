e37c1ab7457a16aaabbfe2a651d9e4ceb7887719
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2022-11-17 16:24:46 -0500

    tops class

--- src/Store/tops.java
+++ src/Store/tops.java
@@ -2,8 +2,24 @@ package Store;
 //import oop.*;
 
 public class Tops extends Clothing {
+    private String size;
+    private String type;
 
-    public Tops(String clothType, String clothColour, double clothPrice, int topSize, String topType){
+    public Tops(String clothType, String clothColour, double clothPrice, String topSize, String topType){
         super(clothType, clothColour, clothPrice);  
+        this.size = topSize;
+        this.type = topType;
+    }
+
+    public String getSize(){
+        return this.size;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public String toString(){
+        return "...";
     }
 }
