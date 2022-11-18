5d76edc0c9e905523258432cd9827ca214f5687d
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2022-11-17 23:14:41 -0500

    user input for clothes

--- src/Store/main.java
+++ src/Store/main.java
@@ -1,6 +1,7 @@
 package Store;
 import java.io.*;
 import java.util.ArrayList;
+import java.text.DecimalFormat;
 //import oop.*;
 
 
@@ -11,9 +12,50 @@ public class main {
 
     public static void main(String[] args) throws IOException {
 
-        // declare objects
+        // create colour array for clothes
+
+
+        // declare objects available for customers to buy
+        Customer newCustomer;
+        shoppingCart newCart;
+            //shoes (running, boots, sandals)
+        Shoes runningShoes;
+        Shoes bootShoes;
+        Shoes sandalShoes;
+            //pants (cargo, sweatpants, shorts)
+        Pants cargoPants;
+        Pants sweatPants;
+        Pants shortPants;
+            //tops(tanktop, tshirt, longsleeve)
+        Tops tankTops;
+        Tops shortTops;
+        Tops longTops;
         
-        // declare variables
+        // declare variables 
+        String user;
+        String email;
+        String page;
+        boolean isShopping = true;
+        String clothingType;
+
+        String type;
+        String colour;
+        double price;
+
+        int pSize;
+        String pType;
+
+        String tSize;
+        String tType;
+
+        double sSize;
+        String sModel;
+
+        int numberOfClothes = 0;
+        double totalPrice = 0.00;
+        boolean cartEmpty = true;
+        ArrayList<Clothing> listClothes = null;
+        DecimalFormat priceFormat = new DecimalFormat("0.00");
 
         // ask user for information
         System.out.println("----------------------------------------------------");
@@ -22,14 +64,76 @@ public class main {
 
         System.out.println("\n To continue shopping please fill out your information");
         System.out.print("\n Name: ");
-        String username = keyboard.readLine();
+        user = keyboard.readLine();
         System.out.print("\n Email: ");
-        String email = keyboard.readLine();
+        email = keyboard.readLine();
 
-        Customer newCustomer = new Customer(username, email);
+        // creating user
+        newCustomer = new Customer(user, email);
+        newCart = new shoppingCart(email, numberOfClothes, totalPrice, cartEmpty, listClothes);
 
         System.out.println("\n-------------------------");
         System.out.println("Welcome " + newCustomer.getName());
+
+        System.out.println("Now you can start SHOPPING!");
+
+        // print out what the user is able to do
+        System.out.println("\nPage: \n- Shop\n- View cart");
+        
+
+        // loop while shopping
+
+        if (isShopping == true);
+
+            System.out.print("\nChoose an page: ");
+            page = keyboard.readLine();
+
+            if(page.equalsIgnoreCase("Shop")){
+                // ask if user wants to shops in shoes, tops, or pants
+                System.out.println("\nClothing Type: \n- Pants \n- Tops \n- Shoes");
+                System.out.print("\nPick a clothing type listed above: ");
+                clothingType = keyboard.readLine();
+
+                // based on what type of clothing was chosen list what is available
+                if (clothingType.equalsIgnoreCase("Pants")){
+                    //(cargo, sweatpants, shorts)
+                    System.out.println("\nPants Options: \n- Cargo Pants \n- Sweatpants \n- Shorts");
+                    System.out.print("\nChoose from pants: ");
+                    pType = keyboard.readLine();
+
+                    System.out.println("Colour options: white, black, beige, green");
+                    System.out.print("Choose a colour for your pants: ");
+
+
+                }
+                else if (clothingType.equalsIgnoreCase("Tops")){
+                    // tanks, short sleeve, long sleeve
+                    System.out.println("\nTops Options: \n- TankTops \n- short sleeves \n- Long sleeves");
+                    System.out.print("\nChoose from tops: ");
+                    tType = keyboard.readLine();
+
+                    System.out.println("Colour options: white, black, pink, brown");
+                    System.out.print("Choose a colour for your pants: ");
+
+
+
+                }
+                else if (clothingType.equalsIgnoreCase("Shoes")){
+                    // (running, boots, sandals)
+                    System.out.println("\nShoes Options: \n- Running \n- Boots \n- Sandals");
+                    System.out.print("\nChoose from Shoes: ");
+                    sModel = keyboard.readLine();
+
+                    System.out.println("Colour options: white, black, blue, red");
+                    System.out.print("Choose a colour for your pants: ");
+
+                }
+            }
+
+
+            if(page.equalsIgnoreCase("View cart")){
+
+            }
 
     }
 
