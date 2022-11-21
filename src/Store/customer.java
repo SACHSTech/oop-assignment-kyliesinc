74650db19e012751c1ea16c4c311824e84bb4b30
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2022-11-21 13:06:33 -0500

    javadocs

--- src/Store/customer.java
+++ src/Store/customer.java
@@ -1,21 +1,42 @@
 package Store;
-//import oop.*;
 
+/**
+ * Customer class file
+ * 
+ * @author: K. Sinclair
+ * 
+ */
 
 public class Customer {
-     String name;
-     String email;
+    // instance variables
+    String name;
+    String email;
 
-    // get name and cart for each customer
+    /**
+     * Constructor -  creates a new instance for a customer
+     * 
+     * @param newName - the name of the customer
+     * @param newEmail - the customer's email info
+     */
     public Customer(String newName, String newEmail){
         name = newName;
         email = newEmail;
     }
 
+    /**
+     * Returns the customer's name
+     * 
+     * @return String variable name 
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * Returns the customer's email
+     * 
+     * @return String variable email
+     */
     public String getEmail(){
         return email;
     }
--- src/Store/tops.java
+++ src/Store/tops.java
@@ -1,29 +1,57 @@
 package Store;
-//import oop.*;
 
-import java.util.ArrayList;
+/**
+ * Shoes class file
+ * @author: K. Sinclair
+ * 
+ */
 
 public class Tops extends Clothing {
+    // instance variables
     private String tSize;
     private String tType;
 
+    /**
+     * Constructor - creates a new instance of Tops
+     * 
+     * @param clothType - represents the type: Tops
+     * @param clothColour - the colour of the top
+     * @param clothPrice - the price of the top
+     * @param topSize - the size of top: small, medium, large
+     * @param topType - the type of top: tanktop, short sleeve, long sleeve
+     */
     public Tops(String clothType, String clothColour, double clothPrice, String topSize, String topType){
-        super(clothType, clothColour, clothPrice);  
+        // call to super constructor
+        super(clothType, clothColour, clothPrice); 
+
         this.tSize = topSize;
         this.tType = topType;
         
     }
 
+    /**
+     * Returns the size of the top
+     * 
+     * @return String variable tSize, which represents the size of the tops
+     */
     public String getSize(){
         return this.tSize;
     }
 
+    /**
+     * Returns the type of top
+     * 
+     * @return String variable tType, which represents the type of top
+     */
     public String getType(){
         return this.tType;
     }
 
-    
-
+    /**
+     * String representation of the top
+     * 
+     * @return print line description of the top
+     */
     public String toString(){
         return ("Tops description: " + tType + ", " + tSize);
     }
